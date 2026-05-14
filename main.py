8596e533bd8fe81b18a94cb0c745a0b9a89e6e19
Chuuo3o666 <pepesunnytomboy@outlook.com>
2026-05-14 04:16:30 -0600

    Add PostgreSQL

--- main.py
+++ main.py
@@ -1,24 +1,69 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://usuarios_db_9x33_user:pajNkphKikCMzRsu7A3Md1rn9UUASFPM@dpg-d82puc4vikkc73ahvrcg-a/usuarios_db_9x33"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
 
 app = FastAPI()
 
-usuarios = []
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String)
+    edad = Column(Integer)
+
+
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def inicio():
     return {
-        "mensaje": "API funcionando"
+        "mensaje": "API funcionando con PostgreSQL"
     }
+
+
+@app.post("/usuarios")
+def crear_usuario(usuario: dict):
+
+    db = SessionLocal()
+
+    nuevo_usuario = Usuario(
+        nombre=usuario["nombre"],
+        edad=usuario["edad"]
+    )
+
+    db.add(nuevo_usuario)
+    db.commit()
+
+    return {
+        "mensaje": "Usuario agregado"
+    }
+
 
 @app.get("/usuarios")
 def obtener_usuarios():
-    return usuarios
 
-@app.post("/usuarios")
-def crear_usuarios(lista_usuarios: list):
-    usuarios.extend(lista_usuarios)
+    db = SessionLocal()
 
-    return {
-        "mensaje": "Usuarios agregados",
-        "usuarios": lista_usuarios
-    }
+    usuarios = db.query(Usuario).all()
+
+    resultado = []
+
+    for usuario in usuarios:
+        resultado.append({
+            "id": usuario.id,
+            "nombre": usuario.nombre,
+            "edad": usuario.edad
+        })
+
+    return resultado
